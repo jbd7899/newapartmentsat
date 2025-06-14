c3db2b2fdd4c3b6c7564ddab6e30dd195fceffb3
jbd7899 <jbd7899@me.com>
2025-06-13 20:47:11 -0400

    Create lead submissions table if missing

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import { initStorage } from "./storage";
 import { config, validateProductionConfig, addSecurityHeaders } from "./config";
 
 const app = express();
@@ -49,6 +50,8 @@ app.use((req, res, next) => {
   try {
     // Validate production configuration
     validateProductionConfig();
+
+    await initStorage();
 
     const server = await registerRoutes(app);
 
